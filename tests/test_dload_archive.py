7ae6507abc20c4fbed9f91d0820fb302c89bdf6f
forestbat <k678900000@163.com>
2023-08-23 19:05:30 +0800

    change test_dload_archive.py

--- tests/test_dload_archive.py
+++ tests/test_dload_archive.py
@@ -1,5 +1,6 @@
 import platform
 import kaggle
+import json
 
 
 def is_windows():
@@ -12,9 +13,14 @@ def is_64bit_ubuntu():
 
 
 def test_download_archive():
-    api = kaggle.KaggleApi('{"username":"headwater","key":"96e459c7c7353d1233d0d702292f5b0d"}')
+    json_str = '{"username":"headwater","key":"96e459c7c7353d1233d0d702292f5b0d"}'
+    api = kaggle.KaggleApi(json_str)
     api.authenticate()
     if is_64bit_ubuntu():
+        with open('/home/runner/.kaggle/kaggle.json', 'w') as fp:
+            fp.write(json_str)
         api.datasets_download(owner_slug='headwater', dataset_slug='Camels')
     elif is_windows():
+        with open('C:\\Users\\runneradmin\\.kaggle\\kaggle.json', 'w') as fp:
+            fp.write(json_str)
         api.datasets_download(owner_slug='headwater', dataset_slug='Camels')
