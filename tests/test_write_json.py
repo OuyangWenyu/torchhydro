5f3dbec629687b8ccc49e2f1147a89ed58d34fdc
forestbat <k678900000@163.com>
2023-09-17 15:04:53 +0800

    fix w to w+ mode

--- tests/test_write_json.py
+++ tests/test_write_json.py
@@ -13,8 +13,8 @@ def is_64bit_ubuntu():
 def test_wjson():
     json_str = '{"username":"headwater","key":"96e459c7c7353d1233d0d702292f5b0d"}'
     if is_64bit_ubuntu():
-        with open('/home/runner/.kaggle/kaggle.json', 'w') as fp:
+        with open('/home/runner/.kaggle/kaggle.json', 'w+') as fp:
             fp.write(json_str)
     elif is_windows():
-        with open('C:\\Users\\runneradmin\\.kaggle\\kaggle.json', 'w') as fp:
+        with open('C:\\Users\\runneradmin\\.kaggle\\kaggle.json', 'w+') as fp:
             fp.write(json_str)
