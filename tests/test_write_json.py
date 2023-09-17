8e5af4e486581f9e58f1c94c08e2ffd1b3066db5
forestbat <k678900000@163.com>
2023-09-17 15:38:26 +0800

    add os.mkdir before writing

--- tests/test_write_json.py
+++ tests/test_write_json.py
@@ -1,3 +1,4 @@
+import os
 import platform
 
 
@@ -13,8 +14,10 @@ def is_64bit_ubuntu():
 def test_wjson():
     json_str = '{"username":"headwater","key":"96e459c7c7353d1233d0d702292f5b0d"}'
     if is_64bit_ubuntu():
+        os.mkdir("/home/runner/.kaggle")
         with open('/home/runner/.kaggle/kaggle.json', 'w+') as fp:
             fp.write(json_str)
     elif is_windows():
+        os.mkdir("/home/runner/.kaggle")
         with open('C:\\Users\\runneradmin\\.kaggle\\kaggle.json', 'w+') as fp:
             fp.write(json_str)
