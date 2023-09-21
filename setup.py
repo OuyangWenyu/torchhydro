aecf48022ebaa0c1b7b19689d668573a0fe2bf14
Wenyu Ouyang <hust2014owen@gmail.com>
2023-09-21 20:36:43 +0800

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -68,6 +68,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/OuyangWenyu/torchhydro",
-    version='0.0.1',
+    version='0.0.2',
     zip_safe=False,
 )
