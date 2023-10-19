e54eec31bcc1ec0a86fe722f29503991a714ad64
Wenyu Ouyang <hust2014owen@gmail.com>
2023-10-19 21:02:07 +0800

    Bump version: 0.0.2 → 0.0.3

--- torchhydro/__init__.py
+++ torchhydro/__init__.py
@@ -11,7 +11,7 @@ Copyright (c) 2023-2024 Wenyu Ouyang. All rights reserved.
 
 __author__ = """Wenyu Ouyang"""
 __email__ = "wenyuouyang@outlook.com"
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 from .configs import *
 from .datasets import *
