74bc661005e8a257c966121802034beaf65d39d6
ouyangwenyu <wenyuouyang@outlook.com>
2023-11-29 17:28:06 +0800

    recover data path settings for test files

--- tests/test_train_camels_lstm.py
+++ tests/test_train_camels_lstm.py
@@ -1,10 +1,10 @@
 """
 Author: Wenyu Ouyang
 Date: 2023-07-25 16:47:19
-LastEditTime: 2023-11-20 19:25:01
+LastEditTime: 2023-11-29 17:27:01
 LastEditors: Wenyu Ouyang
 Description: Test a full training and evaluating process
-FilePath: /torchhydro/tests/test_train_camels_lstm.py
+FilePath: \torchhydro\tests\test_train_camels_lstm.py
 Copyright (c) 2023-2024 Wenyu Ouyang. All rights reserved.
 """
 import os
@@ -18,7 +18,7 @@ from torchhydro.trainers.trainer import train_and_evaluate
 def config():
     project_name = "test_camels/exp1"
     source_path = os.path.join(
-        hds.ROOT_DIR, "waterism", "datasets-origin", "camels", "camels_us"
+        hds.ROOT_DIR, "camels", "camels_us"
     )
     config_data = default_config_file()
     args = cmd(
