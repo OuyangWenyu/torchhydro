45942e627da7a5aea3dcb8f0703834c2113814d8
shouz <thetempmail@163.com>
2025-03-07 17:15:33 +0800

    add routing into forward function, add DplAnnModuleSac module.

--- torchhydro/models/dpl4sac.py
+++ torchhydro/models/dpl4sac.py
@@ -6,6 +6,7 @@ from torch import Tensor
 from torch.nn import functional as F
 from torchhydro.configs.model_config import MODEL_PARAM_DICT, MODEL_PARAM_TEST_WAY
 from torchhydro.models.simple_lstm import SimpleLSTM
+from torchhydro.models.ann import SimpleAnn
 
 PRECISION = 1e-5
 
@@ -131,16 +132,14 @@ class Sac4DplWithNnModule(nn.Module):
     """
     Sacramento model for differential parameter learning with neural network as submodule
     """
-
     def __init__(
         self,
-        kernel_size: int,
         warmup_length: int,
         nn_module=None,
         param_var_index=None,
         source_book="HF",
-        source_type="sources",
-        et_output=1,
+        source_type="sources",  # what's mean?
+        et_output=3,
         nn_hidden_size: Union[int, tuple, list] = None,
         nn_dropout=0.2,
         param_test_way=MODEL_PARAM_TEST_WAY["time_varying"],  # todo:
@@ -149,8 +148,6 @@ class Sac4DplWithNnModule(nn.Module):
         Initiate a Sacramento model instance
         Parameters
         ----------
-        kernel_size
-            the time length of unit hydrograph 单位线
         warmup_length
             the length of warmup periods; 预热期
             sac needs a warmup period to generate reasonable initial state values 需要预热，形成初始条件
@@ -196,10 +193,9 @@ class Sac4DplWithNnModule(nn.Module):
         self.ke_scale = param_range["KE"]
         self.xe_scale = param_range["XE"]
 
-        self.kernel_size = kernel_size  # the unit-line kernel size #
         self.warmup_length = warmup_length
         # there are 2 input variables in Sac: P and PET
-        self.feature_size = 2
+        self.feature_size = 2  # P and Pet are two feature in nn model
         self.source_book = source_book
         self.source_type = source_type
         self.et_output = et_output
@@ -210,13 +206,16 @@ class Sac4DplWithNnModule(nn.Module):
 
     def forward(self, p_and_e, parameters, return_state=False):
         """
-        run sac model
-        forward transmission,
+        sac model
+        forward transmission
 
         Parameters
         ----------
-        p_and_e
-            precipitation and potential evapotranspiration, mm/d.
+        --hydrodata--
+        prcp
+            basin mean precipitation, mm/d.
+        pet
+            potential evapotranspiration, mm/d.
         parameters
         --model parameters--
         kc
@@ -240,44 +239,57 @@ class Sac4DplWithNnModule(nn.Module):
             free water accumulation in the upper layer, mm.
         lztw
             tension water accumulation in the lower layer, mm.
-        --hydrodata--
-        prcp
-            basin mean precipitation, mm.
-        pet
-            potential evapotranspiration, mm.
-        return_state
-            if True, return state values, mainly for warmup periods
+
+
+        hydrodt
+            the time step
+        area
+            basin area, km^2.
+        rivernumber
+            the river sections number
+        rs
+            surface runoff, mm.
+        ri
+            interflow runoff, mm.
+        rgs
+            speedy groundwater runoff, mm.
+        rgp
+            slow groundwater runoff, mm.
+        q_sim_0
 
         Returns
         -------
-        torch.Tensor
-            streamflow got by XAJ
-        q_sim
-            the simulate flow, Q(m^3/s).
-        es
-            the simulate evaporation, E(mm/d)
+        e_sim : torch.Tensor
+            the simulated evaporation, E(mm/d).
+        q_sim : torch.Tensor
+            the simulated flow, Q(m^3/s).
         """
         sac_device = p_and_e.device
         prcp = torch.clamp(p_and_e[:, 0], min=0.0)
         pet = torch.clamp(p_and_e[:, 1], min=0.0)
         # parameters
         kc = self.kc_scale[0] + parameters[:, 0] * (self.kc_scale[1] - self.kc_scale[0])
-        pctim = self.pctiscale[1] + parameters[:, 1] * (self.pctiscale[1] - self.pctiscale[0])
-        adimp = self.adimscale[1] + parameters[:, 1] * (self.adimscale[1] - self.adimscale[0])
-        uztwm = self.uztwscale[2] + parameters[:, 2] * (self.uztwscale[2] - self.uztwscale[0])
-        uzfwm = self.uzfwscale[3] + parameters[:, 3] * (self.uzfwscale[3] - self.uzfwscale[0])
-        lztwm = self.lztwscale[4] + parameters[:, 4] * (self.lztwscale[4] - self.lztwscale[0])
-        lzfsm = self.lzfsscale[5] + parameters[:, 5] * (self.lzfsscale[5] - self.lzfsscale[0])
-        lzfpm = self.lzfpscale[6] + parameters[:, 6] * (self.lzfpscale[6] - self.lzfpscale[0])
-        rserv = self.rserv_scale[7] + parameters[:, 7] * (self.rserv_scale[7] - self.rserv_scale[0])
-        pfree = self.pfree_scale[8] + parameters[:, 8] * (self.pfree_scale[8] - self.pfree_scale[0])
-        riva = self.riva_scale[9] + parameters[:, 9] * (self.riva_scale[9] - self.riva_scale[0])
-        zperc = self.zperc_scale[10] + parameters[:, 10] * (self.zperc_scale[10] - self.zperc_scale[0])
-        rexp = self.rexscale[11] + parameters[:, 11] * (self.rexscale[11] - self.rexscale[0])
-        uzk = self.uzk_scale[12] + parameters[:, 12] * (self.uzk_scale[12] - self.uzk_scale[0])
-        lzsk = self.lzsk_scale[13] + parameters[:, 13] * (self.lzsk_scale[13] - self.lzsk_scale[0])
-        lzpk = self.lzpk_scale[14] + parameters[:, 14] * (self.lzpk_scale[14] - self.lzpk_scale[0])
-        # middle variable  # todo: use warmup_length to handling the initial condition
+        pctim = self.pctiscale[0] + parameters[:, 1] * (self.pctiscale[1] - self.pctiscale[0])
+        adimp = self.adimscale[0] + parameters[:, 2] * (self.adimscale[1] - self.adimscale[0])
+        uztwm = self.uztwscale[0] + parameters[:, 3] * (self.uztwscale[1] - self.uztwscale[0])
+        uzfwm = self.uzfwscale[0] + parameters[:, 4] * (self.uzfwscale[1] - self.uzfwscale[0])
+        lztwm = self.lztwscale[0] + parameters[:, 5] * (self.lztwscale[1] - self.lztwscale[0])
+        lzfsm = self.lzfsscale[0] + parameters[:, 6] * (self.lzfsscale[1] - self.lzfsscale[0])
+        lzfpm = self.lzfpscale[0] + parameters[:, 7] * (self.lzfpscale[1] - self.lzfpscale[0])
+        rserv = self.rserv_scale[0] + parameters[:, 8] * (self.rserv_scale[1] - self.rserv_scale[0])
+        pfree = self.pfree_scale[0] + parameters[:, 9] * (self.pfree_scale[1] - self.pfree_scale[0])
+        riva = self.riva_scale[0] + parameters[:, 10] * (self.riva_scale[1] - self.riva_scale[0])
+        zperc = self.zperc_scale[0] + parameters[:, 11] * (self.zperc_scale[1] - self.zperc_scale[0])
+        rexp = self.rexscale[0] + parameters[:, 12] * (self.rexscale[1] - self.rexscale[0])
+        uzk = self.uzk_scale[0] + parameters[:, 13] * (self.uzk_scale[1] - self.uzk_scale[0])
+        lzsk = self.lzsk_scale[0] + parameters[:, 14] * (self.lzsk_scale[1] - self.lzsk_scale[0])
+        lzpk = self.lzpk_scale[0] + parameters[:, 15] * (self.lzpk_scale[1] - self.lzpk_scale[0])
+        ci = self.ci_scale[0] + parameters[:, 16] * (self.ci_scale[1] - self.ci_scale[0])
+        cgs = self.cgs_scale[0] + parameters[:, 17] * (self.cgs_scale[1] - self.cgs_scale[0])
+        cgp = self.cgp_scale[0] + parameters[:, 18] * (self.cgp_scale[1] - self.cgp_scale[0])
+        ke = self.ke_scale[0] + parameters[:, 19] * (self.ke_scale[1] - self.ke_scale[0])
+        xe = self.xe_scale[0] + parameters[:, 20] * (self.xe_scale[1] - self.xe_scale[0])
+        # middle variable       # todo: use warmup_length to handling the initial condition
         auztw = 0
         alztw = 0
         uztw = 0
@@ -285,30 +297,35 @@ class Sac4DplWithNnModule(nn.Module):
         lztw = 0
         lzfs = 0
         lzfp = 0
+        qi0 = 0
+        qgs0 = 0
+        qgp0 = 0
+        qs0 = 0
+        # todo:
+        area = 0
+        rivernumber = 0
+        hydrodt = 0
 
         # evaporation
-        # average evaporation of basin
-        ep = kc * pet
-        # runoff of the permanent impervious area
-        roimp = prcp * pctim
-        # evaporation of the permanent impervious area
-        ae2 = pctim * ep
+        ep = kc * pet       # average evaporation of basin
+        roimp = prcp * pctim        # runoff of the permanent impervious area
+        ae2 = pctim * ep        # evaporation of the permanent impervious area
         # evaporation of the alterable impervious area
-        ae1 = torch.min((auztw, ep * (auztw / uztwm)))  # upper layer
-        ae3 = (ep - ae1) * (alztw / (uztwm + lztwm)) # lower layer
+        ae1 = torch.min((auztw, ep * (auztw / uztwm)))      # upper layer
+        ae3 = (ep - ae1) * (alztw / (uztwm + lztwm))        # lower layer
         pav = torch.max((0.0, prcp - (uztwm - (auztw - ae1))))
         adsur = pav * ((alztw - ae3) / lztwm)
         ars = torch.max((0.0, ((pav - adsur) + (alztw -ae3)) - lztwm))
         auztw = torch.min((uztwm, (auztw - ae1) + prcp))
         alztw = torch.min((lztwm, (pav - adsur) + (alztw - ae3)))
         # evaporation of the permeable area
-        e1 = torch.min((uztw, ep * (uztw / uztwm)))  # upper layer
-        e2 = torch.min((uzfw, ep - e1))  # lower layer
-        e3 = (ep - e1 - e2) * (lztw / (uztwm + lztwm))  # deeper layer
+        e1 = torch.min((uztw, ep * (uztw / uztwm)))     # upper layer
+        e2 = torch.min((uzfw, ep - e1))     # lower layer
+        e3 = (ep - e1 - e2) * (lztw / (uztwm + lztwm))      # deeper layer
         lt = lztw - e3
-        e4 = riva * ep  # river net, lakes and hydrophyte
+        e4 = riva * ep      # river net, lakes and hydrophyte
         # total evaporation
-        e0 = ae2 + ae1 + ae3 + e1 + e2 + e3 + e4  # the total evaporation
+        e_sim = ae2 + ae1 + ae3 + e1 + e2 + e3 + e4         # the total evaporation
 
         # generate runoff
         # runoff of the alterable impervious area
@@ -319,7 +336,7 @@ class Sac4DplWithNnModule(nn.Module):
         rs = torch.max((prcp + (uztw + uzfw - e1 - e2) - (uztwm + uzfwm), 0.0)) * parea
         ut = torch.min((uztwm, uztw - e1 + prcp))
         uf = torch.min((uzfwm, prcp + (uztw + uzfw - e1 - e2) - ut))
-        ri = uf * uzk  # interflow
+        ri = uf * uzk       # interflow
         uf = uf - ri
         # the infiltrated water
         pbase = lzfsm * lzsk + lzfpm * lzpk
@@ -339,8 +356,8 @@ class Sac4DplWithNnModule(nn.Module):
         lp = lzfp + percp
         # generate groundwater runoff
         rgs = ls * lzsk
+        ls = ls - rgs
         rgp = lp * lzpk
-        ls = ls - rgs  # update
         lp = lp - rgp
         # water balance check
         if ut / uztwm < uf / uzfwm:
@@ -362,12 +379,138 @@ class Sac4DplWithNnModule(nn.Module):
             lzfs = ls
             lzfp = lp
 
-        rs = roimp + (adsur + ars) + rs
-        ri = ri
-        rg = rgs + rgp
+        rs = roimp + (adsur + ars) + rs     # the total surface runoff
 
-        q_sim = calculate_route()
-        return q_sim
+        # routing
+        u = (1 - pctim - adimp) * area * 1000  # todo: / hydrodt   u, the unit converting coefficient.
+        parea = 1 - pctim - adimp
+
+        # slope routing, use the linear reservoir method
+        qs = rs * area * 1000.0         # todo: /hydrodt
+        qi = ci * qi0 + (1 - ci) * ri * u
+        qgs = cgs * qgs0 + (1 - cgs) * rgs * u
+        qgp = cgp * qgp0 + (1 - cgp) * rgp * u
+        q_sim = (qs + qi + qgs + qgp)
+
+        # river routing, use the Muskingum routing method
+        q_sim_0 = qs0 + qi0 + qgs0 + qgp0
+        if rivernumber > 0:
+            dt = hydrodt / 3600.0 / 2.0     # todo: hydrodt, the timestep of data
+            xo = xe
+            ko = ke / rivernumber
+            c1 = ko * (1.0 - xo) + dt
+            c2 = (-ko * xo + dt) / c1
+            c3 = (ko * (1.0 - xo) - dt) / c1
+            c1 = (ko * xo + dt) / c1
+            i1 = q_sim_0
+            i2 = q_sim
+            for i in range(rivernumber):
+                q_sim_0 = qq[i]  # todo:
+                i2 = c1 * i1 + c2 * i2 + c3 * q_sim_0
+                qq[i] = i2
+                i1 = q_sim_0
+        q_sim = i2
+        return e_sim, q_sim
+
+
+class DplAnnModuleSac(nn.Module):
+    """
+    Sacramento differential parameter learning - neural network model
+    """
+    def __init__(
+        self,
+        n_input_features,
+        n_output_features,
+        n_hidden_states,
+        warmup_length,
+        param_limit_func="clamp",
+        param_test_way="final",
+        param_var_index=None,
+        source_book="HF",
+        source_type="source",
+        nn_hidden_states=None,
+        nn_dropout=0.2,
+        et_output=3,
+    ):
+        """
+        Differential Parameter learning model only with attributes as DL model's input: ANN -> Param -> SAC
+
+        The principle can be seen here: https://doi.org/10.1038/s41467-021-26107-z
+
+        Parameters
+        ----------
+        n_input_features
+            the number of input features of ANN
+        n_output_features
+            the number of output features of ANN, and it should be equal to the number of learning parameters in SAC
+        n_hidden_states
+            the number of hidden features of ANN; it could be Union[int, tuple, list]
+        warmup_length
+            the length of warmup periods;
+            hydrologic models need a warmup period to generate reasonable initial state values
+        param_limit_func
+            function used to limit the range of params; now it is sigmoid or clamp function
+        param_test_way
+            how we use parameters from dl model when testing;
+            now we have three ways:
+            1. "final" -- use the final period's parameter for each period
+            2. "mean_time" -- Mean values of all periods' parameters is used
+            3. "mean_basin" -- Mean values of all basins' final periods' parameters is used
+        """
+        super(DplAnnModuleSac, self).__init__()
+        self.dl_model = SimpleAnn(n_input_features, n_output_features, n_hidden_states)
+        self.pb_model = Sac4DplWithNnModule(
+            warmup_length,
+            source_book=source_book,
+            source_type=source_type,
+            nn_hidden_size=nn_hidden_states,
+            nn_dropout=nn_dropout,
+            et_output=et_output,
+            param_var_index=param_var_index,
+            param_test_way=param_test_way,
+        )
+        self.param_func = param_limit_func
+        self.param_test_way = param_test_way
+        self.param_var_index = param_var_index
+
+    def forward(self, x, z):
+        """
+        Differential parameter learning
+
+        z (normalized input) -> ANN -> param -> + x (not normalized) -> gr4j -> q
+        Parameters will be denormalized in gr4j model
+
+        Parameters
+        ----------
+        x
+            not normalized data used for physical model; a sequence-first 3-dim tensor. [sequence, batch, feature]
+        z
+            normalized data used for DL model; a 2-dim tensor. [batch, feature]
+
+        Returns
+        -------
+        torch.Tensor
+            one time forward result
+        """
+        gen = self.dl_model(z)
+        if torch.isnan(gen).any():
+            raise ValueError("Error: NaN values detected. Check your data firstly!!!")
+        # we set all params' values in [0, 1] and will scale them when forwarding
+        if self.param_func == "sigmoid":
+            params = F.sigmoid(gen)
+        elif self.param_func == "clamp":
+            params = torch.clamp(gen, min=0.0, max=1.0)
+        else:
+            raise NotImplementedError(
+                "We don't provide this way to limit parameters' range!! Please choose sigmoid or clamp"
+            )
+        # just get one-period values, here we use the final period's values,
+        # when the MODEL_PARAM_TEST_WAY is not time_varing, we use the last period's values.
+        if self.param_test_way != MODEL_PARAM_TEST_WAY["time_varying"]:
+            params = params[-1, :, :]
+        # Please put p in the first location and pet in the second
+        q, e = self.pb_model(x[:, :, : self.pb_model.feature_size], params)
+        return torch.cat([q, e], dim=-1)
 
 
 class DplLstmNnModuleSac(nn.Module):
@@ -379,7 +522,6 @@ class DplLstmNnModuleSac(nn.Module):
         n_input_features,
         n_output_features,
         n_hidden_states,
-        kernel_size,
         warmup_length,
         param_limit_func="clamp",
         param_test_way="final",
@@ -403,8 +545,6 @@ class DplLstmNnModuleSac(nn.Module):
             the number of output features of LSTM, and it should be equal to the number of learning parameters in SAC
         n_hidden_states
             the number of hidden features of LSTM
-        kernel_size
-            the time length of unit hydrograph
         warmup_length
             the length of warmup periods;
             hydrologic models need a warmup period to generate reasonable initial state values
@@ -423,7 +563,6 @@ class DplLstmNnModuleSac(nn.Module):
         super(DplLstmNnModuleSac, self).__init__()
         self.dl_model = SimpleLSTM(n_input_features, n_output_features, n_hidden_states)
         self.pb_model = Sac4DplWithNnModule(
-            kernel_size,
             warmup_length,
             source_book=source_book,
             source_type=source_type,
