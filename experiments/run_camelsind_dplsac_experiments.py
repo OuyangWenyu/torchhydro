00144b38d6a80d55e0ac57b86d53ed0cdeff48c1
shouz <thetempmail@163.com>
2025-03-26 17:16:16 +0800

    add camelsind_dplsac and camelsse_dplsac, tested both and passed.

--- experiments/run_camelsind_dplsac_experiments.py
+++ experiments/run_camelsind_dplsac_experiments.py
@@ -3,37 +3,48 @@ from torchhydro.configs.config import cmd, default_config_file, update_cfg
 from torchhydro import SETTING
 from torchhydro.trainers.trainer import train_and_evaluate
 
-VAR_C_CHOSEN_FROM_CAMELS_CH = [
+VAR_C_CHOSEN_FROM_CAMELS_IND = [
     "elev_mean",
     "slope_mean",
-    "area",
-    "scrub_perc",  # note: this field in original data file is different with its in data description pdf file, choose the former for convience.
-    "mixed_wood_perc",  # note: this field in original data file is different with its in data description pdf file, choose the former for convience.
-    "rock_perc",
+    "cwc_area",
+    "trees_frac",
+    "lai_max",
+    "lai_diff",
     "dom_land_cover",
-    "crop_perc",
-    "root_depth_50",
-    "root_depth",
-    "porosity",
-    "conductivity",
-    "tot_avail_water",
-    "unconsol_sediments",
-    "siliciclastic_sedimentary",
-    "geo_porosity",
-    "geo_log10_permeability",
+    "dom_land_cover_frac",
+    "crops_frac",
+    "soil_depth",
+    "soil_awc_top",
+    "soil_conductivity_top",
+    "water_frac",
+    "geol_class_1st",
+    "geol_class_2nd",
+    "geol_porosity",
+    "geol_permeability",
 ]
-VAR_T_CHOSEN_FROM_CH = [
-    "precipitation",
-    "ET",
-    "waterlevel",
-    "temperature_min",
-    "temperature_mean",
-    "temperature_max",
-    "rel_sun_dur",
-    # "swe",
+VAR_T_CHOSEN_FROM_IND = [
+    "prcp",
+    "pet",
+    "tmax",
+    "tmin",
+    "tavg",
+    "srad_lw",
+    "srad_sw",
+    "wind_u",
+    "wind_v",
+    "wind",
+    "rel_hum",
+    "pet_gleam",
+    "aet_gleam",
+    "evap_canopy",
+    "evap_surface",
+    "sm_lvl1",
+    "sm_lvl2",
+    "sm_lvl3",
+    "sm_lvl4",
 ]
 
-def run_camelschdplsac(
+def run_camelsinddplsac(
     train_period=None,
     valid_period=None,
     test_period=None
@@ -49,7 +60,7 @@ def run_camelschdplsac(
     -------
 
     """
-    if train_period is None:  # camels-ch time_range: ["1981-01-01", "2020-12-31"]
+    if train_period is None:  # camels-ind time_range: ["1981-01-01", "2020-12-31"]
         train_period = ["2017-10-01", "2018-10-01"]
     if valid_period is None:
         valid_period = ["2018-10-01", "2019-10-01"]
@@ -57,19 +68,19 @@ def run_camelschdplsac(
         test_period = ["2019-10-01", "2020-10-01"]
     config = default_config_file()
     args = cmd(
-        sub=os.path.join("test_camels", "dplsac_lstm_camelsch"),
-        # sub=os.path.join("test_camels", "dplsac_ann_camelsch"),
+        sub=os.path.join("test_camels", "expdpllstmsac_camelsind"),
+        # sub=os.path.join("test_camels", "expdplannsac"),
         source_cfgs={
-            "source_name": "camels_ch",
+            "source_name": "camels_ind",
             "source_path": os.path.join(
-                SETTING["local_data_path"]["datasets-origin"], "camels", "camels_ch"
+                SETTING["local_data_path"]["datasets-origin"], "camels", "camels_ind"
             ),
         },
         ctx=[-1],
         model_name="DplLstmSac",
         # model_name="DplAnnSac",
         model_hyperparam={
-            "n_input_features": len(VAR_T_CHOSEN_FROM_CH)+len(VAR_C_CHOSEN_FROM_CAMELS_CH),  # 8 + 17 = 25
+            "n_input_features": len(VAR_T_CHOSEN_FROM_IND)+len(VAR_C_CHOSEN_FROM_CAMELS_IND),  # 19 + 17 = 36
             "n_output_features": 21,
             "n_hidden_states": 256,
             "warmup_length": 10,
@@ -82,34 +93,38 @@ def run_camelschdplsac(
                 "streamflow",
             ],
             "gamma_norm_cols": [
-                "precipitation",
+                "prcp",
                 "pr",
                 "total_precipitation",
                 "potential_evaporation",
                 "ET",
-                "PET",
+                "pet",
                 "ET_sum",
                 "ssm",
             ],
             "pbm_norm": True,
         },
         gage_id=[
-            # "2009",
-            # "2011",
-            # "2016",
-            # "2018",
-            # "2019",
-            # "2020",
-            # "2024",
-            # "2029",
-            # "2030",
-            # "2033",
-            # "2034",
-            # "2044",
-            "2053",  #
-            # "2067",
-            # "2068",  #
-            # "2070",
+            "03001",
+            "03002",
+            "03003",
+            "03004",
+            "03005",
+            "03006",
+            "03007",
+            "03008",
+            "03009",
+            "03010",
+            "03011",
+            "03012",
+            "03013",
+            "03014",
+            "03015",
+            "03016",
+            "03017",
+            "03018",
+            "03019",
+            "03020",
         ],
         train_period=train_period,
         valid_period=valid_period,
@@ -117,16 +132,16 @@ def run_camelschdplsac(
         batch_size=20,
         forecast_history=0,
         forecast_length=30,
-        var_t=VAR_T_CHOSEN_FROM_CH,
-        var_c=VAR_C_CHOSEN_FROM_CAMELS_CH,
+        var_t=VAR_T_CHOSEN_FROM_IND,
+        var_c=VAR_C_CHOSEN_FROM_CAMELS_IND,
         var_out=["streamflow"],
         target_as_input=0,
         constant_only=0,
-        train_epoch=3,
+        train_epoch=10,
         save_epoch=1,
         model_loader={
             "load_way": "specified",
-            "test_epoch": 3,
+            "test_epoch": 10,
         },
         warmup_length=10,
         opt="Adadelta",
@@ -137,12 +152,10 @@ def run_camelschdplsac(
     print("All processes are finished!")
 
 
-run_camelschdplsac(  # camels-ch time_range: ["1981-01-01", "2020-12-31"]
-    train_period=["1981-01-01", "1982-01-01"],
-    valid_period=["1986-10-01", "1987-10-01"],
-    test_period=["1987-10-01", "1988-10-01"],
+run_camelsinddplsac(  # camels-ind time_range: ["1981-01-01", "2020-12-31"]
+    train_period=["1981-07-01", "1982-07-01"],
+    valid_period=["1982-10-01", "1983-10-01"],
+    test_period=["1983-10-01", "1984-10-01"],
 )
 
-
-# 229
-# 17
+# 99
