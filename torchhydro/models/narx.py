7c78e1db72f8491c9e109200f17e5f607cd41eec
shouz <thetempmail@163.com>
2025-03-31 20:12:10 +0800

    init.

--- torchhydro/models/narx.py
+++ torchhydro/models/narx.py
@@ -1,8 +1,39 @@
 import torch.nn as nn
+from torch.nn.modules.rnn import RNNBase
+from typing import overload
 
-class narx(nn.Module):
+__all__ = [
+    "RNNBase",
+    "narx",
+]
+
+class narx(RNNBase):
     """
     narx model
+    nonlinear autoregressive with exogenous inputs
     """
+    @overload
+    def __init__(
+        self,
+        input_size: int,
+        hidden_size: int,
+        num_layers: int = 1,
+        bias: bool = True,
+        batch_first: bool = False,
+        dropout: float = 0.0,
+        bidirectional: bool = False,
+        proj_size: int = 0,
+        device=None,
+        dtype=None,
+    ) -> None:
+        ...
+
+    @overload
+    def __init__(self, *args, **kwargs):
+        ...
+
+    def __init__(self, *args, **kwargs):
+        super().__init__("narx", *args, **kwargs)
+
     def __init__(self):
         super(narx, self).__init__()
