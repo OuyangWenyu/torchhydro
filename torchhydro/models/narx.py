fbcfe25a6c4d7d8602e3c7f34a72b94a6bbfabc5
shouz <thetempmail@163.com>
2025-04-01 10:56:23 +0800

    add init function.

--- torchhydro/models/narx.py
+++ torchhydro/models/narx.py
@@ -1,16 +1,26 @@
+
+from typing import overload, Optional, Tuple
+
+import torch
 import torch.nn as nn
 from torch.nn.modules.rnn import RNNBase
-from typing import overload
+from torch import Tensor
+from torch.nn.utils.rnn import PackedSequence
 
 __all__ = [
     "RNNBase",
     "narx",
 ]
 
+_narx_impls = {
+    "narx_tanh",
+    "narx_relu",
+}
+
 class narx(RNNBase):
     """
     narx model
-    nonlinear autoregressive with exogenous inputs
+    nonlinear autoregressive with exogenous inputs neural network.
     """
     @overload
     def __init__(
@@ -18,6 +28,7 @@ class narx(RNNBase):
         input_size: int,
         hidden_size: int,
         num_layers: int = 1,
+        nonlinearity: str = "tanh",
         bias: bool = True,
         batch_first: bool = False,
         dropout: float = 0.0,
@@ -33,7 +44,21 @@ class narx(RNNBase):
         ...
 
     def __init__(self, *args, **kwargs):
-        super().__init__("narx", *args, **kwargs)
-
-    def __init__(self):
-        super(narx, self).__init__()
+        if "proj_size" in kwargs:
+            raise ValueError(
+                "proj_size argument is not supported for narx"
+            )
+        if len(args) > 3:
+            self.nonlinearity = args[3]
+            args = args[:3] + args[4:]
+        else:
+            self.nonlinearity = kwargs.pop("nonlinearity", "tanh")
+        if self.nonlinearity == "tanh":
+            mode = "narx_tanh"
+        elif self.nonlinearity == "relu":
+            mode = "narx_relu"
+        else:
+            raise ValueError(
+                f"Unknown nonlinearity '{self.nonlinearity}'. Select from 'tanh' or 'relu'."
+            )
+        super().__init__(mode, *args, **kwargs)
