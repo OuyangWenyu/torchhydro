1800a39d1b468d185fc2ac11e3c988fabf69eb1a
shouz <thetempmail@163.com>
2025-04-08 20:13:51 +0800

    add single_basin.

--- torchhydro/models/basintree.py
+++ torchhydro/models/basintree.py
@@ -34,7 +34,7 @@ class BasinTree:
         -------
 
         """
-        self.nestedness = pd.concat([self.nestedness,pd.DataFrame(columns=["type_single_river", "type_leaf", "type_limb", "type_river_tree_root"])],axis=1,sort=False)
+        self.nestedness = pd.concat([self.nestedness,pd.DataFrame(columns=["type_single_river", "type_leaf", "type_limb", "type_river_tree_root", "basin_type"])],axis=1,sort=False)
 
         type_single_river = [False]*self.n_basin
         n_single_river = 0
@@ -44,6 +44,7 @@ class BasinTree:
         n_limb = 0
         type_river_tree_root = [False]*self.n_basin
         n_river_tree_root = 0
+        basin_type = [""]*self.n_basin
 
         for i in range(self.n_basin):
             nes_n_nested_within = self.nestedness.at[i, 3]
@@ -51,19 +52,24 @@ class BasinTree:
             if nes_n_nested_within == 0 and nes_n_station_ds == 0:
                 type_single_river[i] = True
                 n_single_river = n_single_river + 1
+                basin_type[i] = "single_river"
             if nes_n_nested_within == 0 and nes_n_station_ds > 0:
                 type_leaf[i] = True
                 n_leaf = n_leaf + 1
+                basin_type[i] = "leaf"
             if nes_n_nested_within > 0 and nes_n_station_ds > 0:
                 type_limb[i] = True
                 n_limb = n_limb + 1
+                basin_type[i] = "limb"
             if nes_n_nested_within > 0 and nes_n_station_ds == 0:
                 type_river_tree_root[i] = True
                 n_river_tree_root = n_river_tree_root + 1
+                basin_type[i] = "river_tree_root"
         self.nestedness['type_single_river'] = type_single_river
         self.nestedness['type_leaf'] = type_leaf
         self.nestedness['type_limb'] = type_limb
         self.nestedness['type_river_tree_root'] = type_river_tree_root
+        self.nestedness['basin_type'] = basin_type
         self.n_single_river = n_single_river
         self.n_leaf = n_leaf
         self.n_limb = n_limb
@@ -73,26 +79,64 @@ class BasinTree:
         self,
         basin_id: str = None
     ):
-        basin_up = None
+        basin_us = None
         basin_ds = None
-        return basin_up, basin_id, basin_ds
+        return basin_us, basin_ds
 
     def leaf(
         self,
         basin_id: str = None
     ):
-        basin_up = None
+        basin_us = None
         basin_ds = self.nestedness.at[basin_id, 2]
-        return basin_up, basin_id, basin_ds
+        return basin_us, basin_ds
 
-    def singel_basin(self):
+    def limb(
+        self,
+        basin_id: str = None
+    ):
+        basin_us = self.nestedness.at[basin_id, 5]
+        if not basin_us==None:
+            basin_us = basin_us.split(",")
+            n_basin_us = len(basin_us)
+
+        basin_ds = self.nestedness.at[basin_id, 2]
+        return basin_us, basin_ds
+
+    def river_tree_root(
+        self,
+        basin_id: str = None
+    ):
+        basin_us = self.nestedness.at[basin_id, 5]
+        if not basin_us==None:
+            basin_us = basin_us.split(",")
+            n_basin_us = len(basin_us)
+
+        basin_ds = self.nestedness.at[basin_id, 2]
+        return basin_us, basin_ds
+
+    def single_basin(
+        self,
+        basin_id: str = None,
+    ):
         """
 
         Returns
         -------
 
         """
-
+        basin_type = self.nestedness.at[basin_id, "basin_type"]
+        basin_us = None
+        basin_ds = None
+        if basin_type == "single_river":
+            basin_us, basin_ds = self.single_river(basin_id)
+        if basin_type == "leaf":
+            basin_us, basin_ds = self.leaf(basin_id)
+        if basin_type == "limb":
+            basin_us, basin_ds = self.limb(basin_id)
+        if basin_type == "river_tree_root":
+            basin_us, basin_ds = self.river_tree_root(basin_id)
+        return basin_us, basin_ds
 
     def get_tree(self, basin_id):
         """
