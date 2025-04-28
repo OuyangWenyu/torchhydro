df8ec21d2e617b27d6ac0211fb00a062876ba3e1
shouz <thetempmail@163.com>
2025-04-28 20:16:14 +0800

    add calculate trend and season item.

--- torchhydro/datasets/mi_stl.py
+++ torchhydro/datasets/mi_stl.py
@@ -8,24 +8,75 @@ class STL():
     """
     Seasonal-Trend decomposition using LOESS
     """
-    def __init__(self):
+    def __init__(self, x):
         """
         initiate a STL model
         """
-        self.f = 6  # the frequence of time series
-        self.l = 4 * 30  # the length of time series
+        self.frequency = 6  # the frequency of time series
+        self.length = 4 * 30  # the length of time series
         self.trend = None  # trend item
         self.season = None  # season item
         self.residuals = None  # residuals item
-        self.x = None  # the original data
+        self.x = x  # the original data
         self.mode = "addition"
 
-    def get_scope_t(self, t):
+    def _get_t_range(self, t):
         """get the scope of time period t"""
-        t_start = (self.f+self.l)/2
-        t_end = 1 - (self.f-self.l)/2
-        t_scope = [t_start, t_end]
-        return t_scope
+        t_start = (self.frequency+self.length)/2
+        t_end = 1 - (self.frequency-self.length)/2
+        t_range = [t_start, t_end]
+        return t_range
+
+    def _get_data(self, t_range):
+        """get the data of specified time range"""
+        data = self.x[t_range[0]:t_range[1], :, :]
+        return data
+
+    def _trend_t(self, t):
+        """
+        get the trend of specified time period
+        Parameters
+        ----------
+        x
+
+        Returns
+        -------
+
+        """
+        # length = x.shape[0]   # (time,basin,features)
+        t_range = self._get_t_range(t)
+        data = self._get_data(t_range)
+        # if length != self.length:
+        #     raise ValueError("Length mismatch")
+        trend_t = np.sum(data, axis=2)/self.frequency
+
+        return trend_t
+
+    def _trend(self):
+        """get the trend of series"""
+        trend = []
+        for i in range(self.length):
+            trend_i = self._trend_t(i)
+            trend.append(trend_i)
+        self.trend = np.array(trend)
+
+    def _compound_season(self):
+        """get the compound season"""
+        c_s = self.x - self.trend
+        return c_s
+
+    def _get_n_range(self):
+        """get the number of time period"""
+
+    def _seasonal_t(self, t):
+        """the season of specified time period """
 
 
+    def _season(self):
+        """get the season of series"""
+        c_s = self.x - self.trend
 
+
+    def _residuals(self):
+        """get the residuals of series"""
+        self.residuals = self.x - self.trend - self.season
